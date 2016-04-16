a4868a0d5c3e2341aef14450fd3d15e1ca832a85
hnatchanon <natchanon.h@hotmail.com>
2016-04-16 15:53:35 +0700

    fox

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+    public Transform target;
+    public float zoomLevel = 20f;
+    public float radius = 40f;
+
+    Vector2 XRotation;
+    Vector2 YRotation;
+
+    void LateUpdate() {
+        XRotation = new Vector2(target.right.x, -target.right.z);
+        YRotation = new Vector2(-target.forward.x, target.forward.z);
+    }
+
+    public Vector2 TransformPosition(Vector3 position) {
+        Vector3 offset = position - target.position;
+        Vector2 newPosition = offset.x * XRotation;
+        newPosition += offset.z * YRotation;
+
+        newPosition *= zoomLevel;
+
+        return newPosition;
+    }
+
+    public Vector2 MoveInside(Vector2 point) {
+
+        float m = point.magnitude;
+        Vector2 tmpPoint = point * radius;
+        tmpPoint /= m;
+
+        return tmpPoint;
+    }
+}
